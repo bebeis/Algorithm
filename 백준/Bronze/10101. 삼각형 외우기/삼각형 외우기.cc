77acc439b703d9e8a8f8b48b7e5937ee409c091a
Junseoooo <69619752+DawnGlow@users.noreply.github.com>
2024-03-12 03:02:17 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if ((a + b + c) != 180) {
+        cout << "Error";
+        return 0;
+    }
+    if ((a == b) && (b == c)) {
+        cout << "Equilateral";
+    }
+    else if (a == b || b == c || a == c) {
+        cout << "Isosceles";
+    }
+    else {
+        cout << "Scalene";
+    }
+}
