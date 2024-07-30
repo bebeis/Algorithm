8714976a0585a33b671630c6f7babe6bb599ab8e
Lumana <69619752+DawnGlow@users.noreply.github.com>
2024-07-31 04:19:58 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -4,28 +4,7 @@ using namespace std;
 int arr[500002];
 int n;
 
-// 이분 탐색 공부를 위해 lower_bound와 upper_bound 라이브러리 함수를 직접 구현 해봄
-int lower_bnd(int target, int len) {
-    int low = 0;
-    int high = len;
-    while (low < high) {
-        int mid = (low + high) / 2;
-        if (arr[mid] >= target) high = mid;
-        else low = mid + 1;
-    }
-    return low;
-}
-
-int upper_bnd(int target, int len) {
-    int low = 0;
-    int high = len;
-    while (low < high) {
-        int mid = (low + high) / 2;
-        if (arr[mid] > target) high = mid;
-        else low = mid + 1;
-    }
-    return low;
-}
+// 라이브러러 써서 풀기
 
 int main(void) {
     ios::sync_with_stdio(false);
@@ -37,6 +16,6 @@ int main(void) {
     cin >> m;
     while (m--) {
         cin >> t;
-        cout << upper_bnd(t, n) - lower_bnd(t, n) << ' ';
+        cout << upper_bound(arr, arr + n, t) - lower_bound(arr, arr + n, t) << ' ';
     }
 }
