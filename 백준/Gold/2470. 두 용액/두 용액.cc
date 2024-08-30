3e6d3f5d9f7b1260110191079a8c82c6632e85fe
Lumana <69619752+DawnGlow@users.noreply.github.com>
2024-08-30 18:46:46 +0900

    [Gold V] Title: 두 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+long long MIN = INT_MAX;
+int arr[100001];
+
+int main(void) {
+    cin.tie(0)->sync_with_stdio(false);
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+    int i = 0, j = n - 1;
+    pair<int, int> result;
+    while (i < j) {
+        long long sum = arr[i] + arr[j];
+        if (abs(MIN) > abs(sum)) {
+            MIN = sum;
+            result = {arr[i], arr[j]};
+        }
+        if (sum < 0) i++;
+        else if (sum > 0) j--;
+        else break;
+    }
+    cout << result.first << ' ' << result.second;
+}
