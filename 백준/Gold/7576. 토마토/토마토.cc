c55700e887f081949e3c215b9e7091a15d73a0f9
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-01-29 01:09:16 +0900

    [Gold V] Title: 토마토, Time: 80 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,61 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 최소 일수 --> BFS
-// 마지막 순회에서 거리 값을 출력하면 되는데 굳이 배열 내에서 최댓값을 찾다가 시간초과-> 2트
-int adj[1001][1001];
-bool vis[1001][1001];
+// O(N^2logn)이하로 풀이
+
+int m, n;
+int board[1001][1001];
 int dist[1001][1001];
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int n, m, last = 0;
-    pair<int, int> cur;
+    cin.tie(0)->sync_with_stdio(false);
     queue<pair<int, int>> Q;
     cin >> m >> n;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cin >> adj[i][j];
+            cin >> board[i][j];
+            if (board[i][j] == 1) Q.push({i, j});
         }
     }
 
-    // BFS 출발 점 체크
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (adj[i][j] == 1 && !vis[i][j]) {
-                Q.push({i, j});
-            }
-        }
-    }
-
-    // BFS
     while (!Q.empty()) {
-        cur = Q.front();
+        auto [cx, cy] = Q.front();
         Q.pop();
 
         for (int i = 0; i < 4; i++) {
-            int nx = cur.first + dx[i];
-            int ny = cur.second + dy[i];
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+
             if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if (!vis[nx][ny] && adj[nx][ny] == 0) {
-                vis[nx][ny] = 1;
-                dist[nx][ny] = dist[cur.first][cur.second] + 1;
+            if (board[nx][ny] == 0) {
+                board[nx][ny] = 1;
+                dist[nx][ny] = dist[cx][cy] + 1;
                 Q.push({nx, ny});
             }
         }
     }
-
-    // 토마토가 모두 익지 못하는 상황을 체크
+    int max = -1;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (!vis[i][j] && !adj[i][j]) {
+            if (board[i][j] == 0) {
                 cout << -1;
                 return 0;
             }
+            if (dist[i][j] > max) max = dist[i][j];
         }
     }
-    cout << dist[cur.first][cur.second];
+    cout << max;
 }
