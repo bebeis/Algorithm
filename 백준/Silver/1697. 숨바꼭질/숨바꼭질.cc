5e2d050b4b57b7d88a90731ff27d8bf1e760d4ee
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-01-30 00:18:53 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,44 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 가장 빠른 시간 구하기 : BFS
-bool vis[100002];
-int dist[100002];
+int n, k;
+int dist[100001];
 
-int back(int x) { return x - 1; }
-int front(int x) { return x + 1; }
-int teleport(int x) { return 2 * x; }
+inline int moveLeft(int x) {return x - 1;}
+inline int moveRight(int x) {return x + 1;}
+inline int teleport(int x) {return x * 2;}
+bool isValidIndex(int x) {
+    if (x < 0) return false;
+    if (x > 100000) return false;
+    return true;
+}
+
+int (*fp[3])(int) = {moveLeft, moveRight, teleport};
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int n, k;
-    queue<int> Q;
-    // 다음 위치를 계산해주는 함수를 편하게 다루기 위해 함수 포인터 배열 사용
-    int (*funcArray[3])(int) = {back, front, teleport};
+    cin.tie(0)->sync_with_stdio(false);
+
     cin >> n >> k;
-
-    dist[n] = 0;
+    if (n == k) {
+        cout << 0;
+        return 0;
+    }
+    queue<int> Q;
     Q.push(n);
-    vis[n] = 1;
-
     while (!Q.empty()) {
         int cur = Q.front();
         Q.pop();
-        int nx;
-
-        // X -1
-        for (int i = 0; i < 3; i++) {
-            nx = funcArray[i](cur);
-            if (nx >= 0 && nx <= 100000) {
-                if (!vis[nx]) {
-                    vis[nx] = 1;
-                    dist[nx] = dist[cur] + 1;
-                    Q.push(nx);
-                }
+        for (auto func : fp) {
+            int nxt = func(cur);
+            if (nxt == k) {
+                cout << dist[cur] + 1;
+                return 0;
+            }
+            if (isValidIndex(nxt) && dist[nxt] == 0) {
+                Q.push(nxt);
+                dist[nxt] = dist[cur] + 1;
             }
         }
     }
-    cout << dist[k];
 }
