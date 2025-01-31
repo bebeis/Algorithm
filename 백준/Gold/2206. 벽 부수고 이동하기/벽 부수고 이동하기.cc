0bf38bf3cbc62885bed6b945c4bd61f4ac18cc51
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-01 02:33:28 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 64 ms, Memory: 12608 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,72 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 내가 떠올린 발상 : 1. 출발점에서 이동할 수 없는 칸까지 거리를 저장하고,
-// 2. 각 이동할 수 없는 칸에서 도착지까지의 거리를 추정
-// 만약 2번과정을 거쳤는데 도착지의 거리가 INT_MAX이면 도착 불가능 한 것, dist[n][m]을 바로 출력(초기값이 -1이라)
 int n, m;
-int adj[1001][1001];
-bool vis[1001][1001];
-
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
+char board[1001][1001]; 
+int dist_visit[1001][1001][2];
+int dx[] = { -1, 0, 1, 0 };
+int dy[] = { 0, 1, 0, -1 };
 
 int main(void) {
-    scanf("%d %d", &n, &m);
-    std::vector<std::vector<int>> dist(1001, std::vector<int>(1001, INT_MAX));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%1d", &adj[i][j]);
+    cin.tie(0)->sync_with_stdio(false);
+    cin >> n >> m;
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> board[i][j];
         }
     }
 
-    queue<pair<int, int>> Q;
-    Q.push({0, 0});
-    vis[0][0] = 1;
-    dist[0][0] = 1;
-    // 벽까지 도달 거리 포함 순회
+    queue<pair<pair<int, int>, bool>> Q;
+    Q.push({{1, 1}, false});
+    dist_visit[1][1][0] = 1; 
+
     while (!Q.empty()) {
-        auto cur = Q.front();
+        int x = Q.front().first.first;
+        int y = Q.front().first.second;
+        bool broken = Q.front().second;
         Q.pop();
 
+        if (x == n && y == m) {
+            cout << dist_visit[x][y][broken];
+            return 0;
+        }
+
         for (int i = 0; i < 4; i++) {
-            int nx = cur.first + dx[i];
-            int ny = cur.second + dy[i];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if (!vis[nx][ny] && adj[nx][ny]) {  // 벽에 도달
-                vis[nx][ny] = 1;
-                dist[nx][ny] = dist[cur.first][cur.second] + 1;
-            } else if (!vis[nx][ny] && !adj[nx][ny]) {  // 벽이 아닌 경우
-                vis[nx][ny] = 1;
-                dist[nx][ny] = dist[cur.first][cur.second] + 1;
-                Q.push({nx, ny});
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            // 배열 범위 체크
+            if (nx < 1 || nx > n || ny < 1 || ny > m) continue;
+
+            if (board[nx][ny] == '1') {
+                if (!broken) {
+                    if (dist_visit[nx][ny][1] == 0) { // 해당 상태를 방문하지 않았다면
+                        dist_visit[nx][ny][1] = dist_visit[x][y][0] + 1;
+                        Q.push({{nx, ny}, true});
+                    }
+                }
+            }
+            else { // 빈 공간인 경우
+                if (dist_visit[nx][ny][broken ? 1 : 0] == 0) { // 현재 상태로 방문하지 않았다면
+                    dist_visit[nx][ny][broken ? 1 : 0] = dist_visit[x][y][broken ? 1 : 0] + 1;
+                    Q.push({{nx, ny}, broken});
+                }
             }
         }
     }
-    // 벽에 도달한 칸을 출발지로 설정
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (vis[i][j] && adj[i][j]) {
-                Q.push({i, j});
-            }
-        }
-    }
-    // 다시 BFS를 도는데, 이동하는 칸이 최단거리가 아닌 경우 순회를 멈춘다.
-    while (!Q.empty()) {
-        auto cur = Q.front();
-        Q.pop();
-        for (int i = 0; i < 4; i++) {
-            int nx = cur.first + dx[i];
-            int ny = cur.second + dy[i];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if (adj[nx][ny]) continue;
-            int ndist = dist[cur.first][cur.second] + 1;
-            if (dist[nx][ny] > ndist) {
-                dist[nx][ny] = ndist;
-                Q.push({nx, ny});
-            }
-        }
-    }
-    if (dist[n - 1][m - 1] == INT_MAX) dist[n - 1][m - 1] = -1;
-    printf("%d", dist[n - 1][m - 1]);
+
+    // 도달 불가능한 경우
+    cout << -1;
 }
