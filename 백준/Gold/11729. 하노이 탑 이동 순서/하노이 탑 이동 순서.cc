2916cb613a70c67f0aee5a029e121e07ee083e24
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-03 00:27:29 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 124 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,36 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-string fullString;
-int count;
+// 1 ~ N을 완성하기 위해서는, 1 ~ N - 1이 완성되어야 한다.
+// -> 재귀 느낌이 난다.
+// 재귀에서 상태 -> 파라미터화
+// 상태 : N - 1, 어디서부터, 어디로
+// n <= 20, O(2^n)은 가능
 
-void hanoi(int bottom, int source, int dest) {
-    int new_dest;
-    for (int i = 1; i <= 3; i++) {
-        if (source != i && dest != i) {
-            new_dest = i;
-        }
-    }
-    if (bottom < 2) {
-        fullString += (to_string(source) + " " + to_string(dest) + '\n');
-        ::count++;
-        return;
-    }
-    hanoi(bottom - 1, source, new_dest);
-    fullString += (to_string(source) + " " + to_string(dest) + '\n');
-    ::count++;
-    hanoi(bottom - 1, new_dest, dest);
+// 틀린 이유: 총 이동횟수 안구해서 틀림;
+
+int n;
+
+// k - 1번이 x에서 y로 움직이고, 바닥이 움직이고, 다시 4번이 y에서 z로움직인다.
+void hanoi(int k, int from, int to) { 
+	if (k == 0) return;
+	int middle = 6 - from - to;
+	hanoi(k - 1, from, middle);
+	cout << from << " " << to << '\n';
+	hanoi(k - 1, middle, to);
 }
 
+
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ::count = 0;
-    ::fullString = "";
-    int n;
-    cin >> n;
-    hanoi(n, 1, 3);
-    cout << ::count << '\n';
-    cout << ::fullString;
+	cin.tie(0)->sync_with_stdio(false);
+	cin >> n;
+    cout << (1 << n) - 1 << '\n';
+	hanoi(n, 1, 3);
 }
