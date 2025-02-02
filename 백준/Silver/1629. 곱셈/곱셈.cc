070d629728e40b43cad13344b44fa3c42839cb2c
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-02 23:50:48 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,24 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 메모리 제한 128MB ==> DP로 결과 저장은 불가능
-// 분할 정복으로 접근하지만 최대한 이전 결과를 이용할 수 있도록 구현
-long long remainder(long long a, long long b, long long c) {
-    if (b == 1) {
-        return a % c;
-    }
-    long long result = remainder(a, b >> 1, c);
+// 아무 생각 없이 풀면 O(21억) -> 시간초과
+// log scale로 줄여야 한다.
+// 분할정복 도입!
+
+long long a, b, c;
+
+long long result(long long b) {
+    if (b == 1) return a % c;
+
+    long long mod = result(b / 2);
+    long long temp = (mod * mod) % c;
+
     if (b % 2 == 0) {
-        return result * result % c;
+        return temp;
     } else {
-        return (((result * result) % c) * a) % c;
+        return (temp * (a % c)) % c;
     }
 }
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    long long a, b, c;
+    cin.tie(0)->sync_with_stdio(false);
+
     cin >> a >> b >> c;
-    cout << remainder(a, b, c);
+    cout << result(b);
 }
