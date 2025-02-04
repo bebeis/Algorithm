6dc23cb85c28721ba13975ac313233a3f7bd8dab
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-05 00:42:27 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,42 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[64][64];
 
-void partition(int n, int x, int y) {
-    if (n == 1) {
-        cout << arr[y][x];
-        return;
-    }
-    bool square = true;
-    int first = arr[y][x];
-    for (int i = y; i < y + n; i++) {
-        for (int j = x; j < x + n; j++) {
-            if (first != arr[i][j]) {
-                square = false;
-                break;
+int n;
+char board[66][66];
+
+void partition(int cx, int cy, int length) {
+    char first = board[cx][cy];
+    for (int i = cx; i < cx + length; i++) {
+        for (int j = cy; j < cy + length; j++) {
+            if (board[i][j] != first) {
+                int halfLength = length / 2;
+                cout << '(';
+                partition(cx, cy, halfLength);
+                partition(cx, cy + halfLength, halfLength);
+                partition(cx + halfLength, cy, halfLength);
+                partition(cx + halfLength, cy + halfLength, halfLength);
+                cout << ')';
+                return;
             }
         }
     }
-    if (square) {
-        cout << first;
-    } else {
-        cout << '(';
-        partition(n / 2, x, y);
-        partition(n / 2, x + n / 2, y);
-        partition(n / 2, x, y + n / 2);
-        partition(n / 2, x + n / 2, y + n / 2);
-        cout << ')';
-    }
+    cout << first;
 }
 
 int main(void) {
-    int n;
+    cin.tie(0)->sync_with_stdio(false);
     cin >> n;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%1d", &arr[i][j]);
-        }
-        cin.get();
+        for (int j = 0; j < n; j++) cin >> board[i][j];
     }
-    partition(n, 0, 0);
+    partition(0, 0, n);
 }
