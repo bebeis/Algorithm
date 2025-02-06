6a197480b234c2648d3243119ea1cc3f6eca16c1
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-07 00:21:56 +0900

    [Silver III] Title: N과 M (3), Time: 340 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 중복순열
-
 int n, m;
-int arr[9];
+int arr[10];
 
-void permuRepeat(int target) {
-    if (target > m) {
-        for (int i = 1; i <= m; i++) cout << arr[i] << " ";
+void backtracking(int cur) {
+    if (cur == m) {
+        for (int i = 0; i < m; i++) cout << arr[i] << ' ';
         cout << '\n';
         return;
     }
+
     for (int i = 1; i <= n; i++) {
-        arr[target] = i;
-        permuRepeat(target + 1);
+        arr[cur] = i;
+        backtracking(cur + 1);
     }
 }
 
 int main(void) {
     cin.tie(0)->sync_with_stdio(false);
     cin >> n >> m;
-    permuRepeat(1);
+
+    backtracking(0);
 }
