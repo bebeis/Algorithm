df2c3b8475340abfe17b6a56098836ecf1bd13bd
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-07 00:04:34 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,28 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
 int arr[10];
-bool visited[10];
-
-void comb(int target, int start) {
-    // 기저 조건
-    if (target > m) {
-        for (int i = 1; i <= m; i++) cout << arr[i] << " ";
-        cout << '\n';
-        return;
-    }
-    for (int i = start; i <= n; i++) {
-        if (visited[i]) continue;
-        arr[target] = i;
-        visited[i] = 1;
-        comb(target + 1, i + 1);
-        visited[i] = 0;
-    }
-}
+int n, m;
 
 int main(void) {
-    cin.tie(0)->sync_with_stdio(false);
     cin >> n >> m;
-    comb(1, 1);
+
+    for (int i = 0; i < m; i++) arr[i] = 0;
+    for (int i = m; i < n; i++) arr[i] = 1;
+    do {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) cout << i + 1 << ' ';
+        }
+        cout << '\n';
+    } while (next_permutation(arr, arr + n));
 }
