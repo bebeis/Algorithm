614def2caea14d276bdd9c5661b4e4c4240a9782
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-13 03:28:16 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -1,38 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int d[1000001];
-int pre[1000001];
+int dp[1000003];
+int before[1000003];
+int n;
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int n;
+    cin.tie(0)->sync_with_stdio(false);
     cin >> n;
-    // tabulation을 이용 --> for문 또는 while문으로 논리 구현
+    dp[1] = 0;
+    before[1] = 0;
     for (int i = 2; i <= n; i++) {
-        d[i] = d[i - 1] + 1;
-        pre[i] = i - 1;
-        if (i % 3 == 0) {
-            int val = d[i / 3] + 1;
-            if (d[i] > val) {
-                d[i] = val; 
-                pre[i] = i / 3;
-            }
+        dp[i] = dp[i - 1] + 1;
+        before[i] = i - 1;
+        if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) {
+            dp[i] = dp[i / 2] + 1;
+            before[i] = i / 2;
         }
-        if (i % 2 == 0){
-            int val = d[i / 2] + 1;
-            if (d[i] > val) {
-                d[i] = val;
-                pre[i] = i / 2;
-            }
+        if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) {
+            dp[i] = dp[i / 3] + 1;
+            before[i] = i / 3;
         }
     }
-    cout << d[n] << '\n';
-    int k = n;
-    while (k != 1) {
-        cout << k << " ";
-        k = pre[k];
+    cout << dp[n] << '\n';
+    while (n != 0) {
+        cout << n << ' ';
+        n = before[n];
     }
-    cout << 1;
 }
