8e9c47b844fbb1ee03f7d8568798429f89c9fc14
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-12 23:50:29 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,10 @@
+-- 조회수가 가장 높은 중고거래 게시물에 대한 첨부파일 경로를 조회
+-- 첨부파일 경로는 FILE ID를 기준으로 내림차순 정렬
+-- 기본적인 파일경로는 /home/grep/src/ 
+-- 게시글 ID를 기준으로 디렉토리가 구분
+-- 파일이름은 파일 ID, 파일 이름, 파일 확장자로 구성
+SELECT CONCAT('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_BOARD NATURAL JOIN USED_GOODS_FILE
+WHERE VIEWS IN (SELECT MAX(VIEWS) AS VIEWS
+FROM USED_GOODS_BOARD)
+ORDER BY FILE_ID DESC
