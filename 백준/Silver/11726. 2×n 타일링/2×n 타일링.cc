5347d1fd03f90653c397803302a6587615e13954
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-13 02:38:19 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 점화식 : d[n] = d[n - 1] + d[n - 2]
-
-int d[1001];
 int n;
+int dp[1002];
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0)->sync_with_stdio(false);
     cin >> n;
-    d[1] = 1; d[2] = 2;
-    for (int i = 3; i <= n; i++) d[i] = (d[i - 1] + d[i - 2]) % 10007;
-    cout << d[n];
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+    }
+    cout << dp[n];
 }
