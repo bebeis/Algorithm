bfb649a6f389f3165347291c19bc56735c4fc833
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-16 05:55:03 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,21 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[51];
-int sum = 0;
-char op[51];
-
 int main(void) {
     cin.tie(0)->sync_with_stdio(false);
-    cin >> arr[0];
-    sum = arr[0];
-    int idx = 0;
+    int x; char c;
+    cin >> x;
+    int sum = x;
     bool minus = false;
-    while (cin >> op[++idx]) {
-        cin >> arr[idx];
-        if (op[idx] == '-') minus = true;
-        if (minus) sum -= arr[idx];
-        else sum += arr[idx];
+    while (cin >> c) {
+        cin >> x;
+        if (c == '-') minus = true;
+        if (minus) sum -= x;
+        else sum += x;
     }
     cout << sum;
 }
