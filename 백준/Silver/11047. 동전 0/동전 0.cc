2c3fcdb91fae103061e3c93f46e8023fbaab3872
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-16 03:18:06 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,26 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// CoinChange Problem에서 동전이 배수 관계--> Greedy로 최적해를 구할 수 있다.
-
-int arr[10];
+int n, k, cnt = 0;
+int coin[12];
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int n, k, max_idx = 0, cnt = 0;
+    cin.tie(0)->sync_with_stdio(false);
+
     cin >> n >> k;
+    int max_idx = 0;
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-        if (arr[i] <= k) {
-            max_idx = i;
-        }
+        cin >> coin[i];
+        if (coin[i] <= k) max_idx = i;
     }
-    while (k) {
-        if (k >= arr[max_idx]) {
-            cnt += k / arr[max_idx];
-            k = k % arr[max_idx];
-        }
+
+    
+    while (k > 0) {
+        cnt += k / coin[max_idx];
+        k %= coin[max_idx];
         max_idx--;
     }
     cout << cnt;
