98c7465f54736bdc40294b86212ea48f14c0d4ec
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-16 00:05:38 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -1,22 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
+// 펜윅트리(BIT) 풀이
+// 수열의 범위가 1000이라서 가능한 풀이
+// 만약 1억이 넘는다면 BIT를 못쓴다.
+const int MAX_A = 1001;
 
-int a[1001];
-int sum[1001];
+
+int query(vector<int>& BIT, int idx) {
+    int res = 0;
+    while(idx > 0) {
+        res = max(res, BIT[idx]);
+        idx -= (idx & -idx);
+    }
+    return res;
+}
+
+void update(vector<int>& BIT, int idx, int val) {
+    while(idx < MAX_A) {
+        BIT[idx] = max(BIT[idx], val);
+        idx += (idx & -idx);
+    }
+}
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0)->sync_with_stdio(false);
+
     int n;
     cin >> n;
-    cin >> a[1];
-    sum[1] = a[1];
-    for (int i = 2; i <= n; i++) {
-        cin >> a[i];
-        sum[i] = a[i];
-        for (int j = i - 1; j >= 1; j--) {
-            if (sum[i] < sum[j] + a[i] && a[j] < a[i]) sum[i] = sum[j] + a[i];
-        }
+    
+    vector<int> A(n);
+    for (int i = 0; i < n; i++){
+        cin >> A[i];
     }
-    cout << *max_element(sum + 1, sum + n + 1);
+    
+    // BIT 배열 초기화 (1-indexed)
+    vector<int> BIT(MAX_A, 0);
+    int answer = 0;
+    
+    for (int i = 0; i < n; i++){
+        // A[i]보다 작은 수들의 dp값의 최댓값을 구함.
+        int best = query(BIT, A[i] - 1) + A[i];
+        update(BIT, A[i], best);
+        answer = max(answer, best);
+    }
+    
+    cout << answer << "\n";
 }
