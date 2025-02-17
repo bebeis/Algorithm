904f2faa11a6876a283fc0405d2b31a0a08f5acc
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-17 23:32:34 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 296 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,21 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[500002];
 int n;
-
-// 라이브러러 써서 풀기
+int arr[500002];
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int m, t;
+    cin.tie(0)->sync_with_stdio(false);
     cin >> n;
     for (int i = 0; i < n; i++) cin >> arr[i];
     sort(arr, arr + n);
-    cin >> m;
+    int m; cin >> m;
     while (m--) {
-        cin >> t;
-        cout << upper_bound(arr, arr + n, t) - lower_bound(arr, arr + n, t) << ' ';
+        int target; cin >> target;
+        cout << upper_bound(arr, arr + n, target) - lower_bound(arr, arr + n, target) << ' ';
     }
 }
