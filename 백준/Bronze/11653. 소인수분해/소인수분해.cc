efdd83d7eb8f04a9d6739e184922351be71dabc8
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-17 21:36:52 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,17 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+int n;
+
 int main(void) {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n, i = 2;
+    cin.tie(0)->sync_with_stdio(false);
     cin >> n;
-    while (n > 1) {
-        if (n % i == 0) {
+
+    for (int i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
             cout << i << '\n';
             n /= i;
         }
-        else {
-            i++;
-        }
     }
+    if (n != 1) cout << n;
 }
