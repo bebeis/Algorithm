004fc192312d685f3ac9cd77c90dc52755be637a
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-20 01:11:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 72 ms, Memory: 6532 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,41 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool vis[1002];
-unordered_set<int> sub;
 vector<int> adj[1002];
+bool vis[1002];
+int n, m;
 
-void DFS(int start) {
-    vis[start] = 1;
-    sub.erase(start);
-    for (int i = 0; i < adj[start].size(); i++) {
-        int next = adj[start][i];
-        if (!vis[next]) {
-            DFS(next);
-        }
+void dfs(int cur) {
+    vis[cur] = 1;
+    for (int nxt : adj[cur]) {
+        if (vis[nxt]) continue;
+        dfs(nxt);
     }
 }
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int n, m, count;
+    cin.tie(0)->sync_with_stdio(false);
     cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int s, e;
-        cin >> s >> e;
-        adj[s].push_back(e);
-        adj[e].push_back(s);
-        sub.insert(s);
-        sub.insert(e);
+    while (m--) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-
-    // 정점이 연결되어 있지 않는 부분도 고려해야 한다
-    count = n - sub.size();
-
-    while (!sub.empty()) {
-        DFS(*sub.begin());
-        count++;
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        if (vis[i]) continue;
+        dfs(i);
+        cnt++;
     }
-    cout << count;
+    cout << cnt;
 }
