6b9d5440e1a0ad986ab48b737fa7708e44361ce4
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-22 07:28:19 +0900

    [Gold IV] Title: 최단경로, Time: 92 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,40 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 방향 그래프
-int v, e, start;
-// (weight, vertex) --> heap에서 weight순으로 정렬될 수 있도록
-vector<pair<int, int>> adj[20002]; 
-int d[20002];
-const int INF = 1e9+10;
+int v, e, st;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // 비용, nxt
+vector<pair<int, int>> adj[20002];
+int dist[20002];
+const int INF = 1e9 + 10;
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    cin >> v >> e >> start;
-    fill(d, d + v + 1, INF);
+    cin.tie(0)->sync_with_stdio(false);
+    cin >> v >> e >> st;
+    for (int i = 1; i <= v; i++) dist[i] = INF;
+    dist[st] = 0;
     while (e--) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        adj[u].push_back({w, v});
+        int a, b, c; cin >> a >> b >> c;
+        adj[a].push_back({c, b});
     }
-    d[start] = 0;
-    pq.push({0, start});
+    pq.push({0, st});
     while (!pq.empty()) {
-        // cw : current weight, cv : current vertex
-        auto [cw, cv] = pq.top();
-        pq.pop();
-        if (d[cv] != cw) continue; // 최소 경로가 아닌 경우
-        for (auto [nw, nv] : adj[cv]) {
-            // nw : next weight, nv : next vertex
-            if (d[nv] <= nw + d[cv]) continue;
-            d[nv] = nw + d[cv];
-            pq.push({d[nv], nv});
+        auto [cc, cur] = pq.top(); pq.pop();
+        if (cc != dist[cur]) continue;
+
+        for (auto [nc, nxt] : adj[cur]) {
+            if (dist[cur] + nc >= dist[nxt]) continue;
+            dist[nxt] = dist[cur] + nc;
+            pq.push({dist[nxt], nxt});
         }
     }
     for (int i = 1; i <= v; i++) {
-        if (d[i] == INF) cout << "INF\n";
-        else cout << d[i] << '\n';
+        if (dist[i] == INF) cout << "INF\n";
+        else cout << dist[i] << '\n';
     }
 }
