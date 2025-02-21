fc6f9f2db9ddf03f215c63b5716e0a973e90d058
Lumana <69619752+DawnGlow@users.noreply.github.com>
2025-02-22 07:09:30 +0900

    [Gold IV] Title: 플로이드, Time: 20 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,42 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 0x3f3f3f3f;
-int d[102][102];
+const int INF = 1e9+10;
+int adj[102][102];
+int n, m;
 
-// 플로이드(All Pair Shortest Path, ASP 문제)
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int n, m, a, b, c;
+    cin.tie(0)->sync_with_stdio(false);
     cin >> n >> m;
-    // 대각선(자기 자신)은 0으로, 나머지는 OF가 발생하지 않는 최댓값으로 초기화
     for (int i = 1; i <= n; i++) {
-        fill(d[i], d[i] + 1 + n, INF);
-        d[i][i] = 0;
+        fill(adj[i] + 1, adj[i] + 1 + n, INF);
+        adj[i][i] = 0;
     }
-    // input
     while (m--) {
-        cin >> a >> b >> c;
-        if (d[a][b] > c) {
-            d[a][b] = c;
-        }
+        int a, b, c; cin >> a >> b >> c;
+        adj[a][b] = min(c, adj[a][b]);
     }
-    // ASP 해결
+
     for (int k = 1; k <= n; k++) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                // D[i][j] = min(D[i][j], D[i][k] + D[k][j])
-                if (d[i][j] > d[i][k] + d[k][j]) {
-                    d[i][j] = d[i][k] + d[k][j];
+                if (adj[i][k] + adj[k][j] < adj[i][j]) {
+                    adj[i][j] = adj[i][k] + adj[k][j];
                 }
             }
         }
     }
-    // output
+
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            cout << ((d[i][j] == INF) ? 0 : d[i][j]) << " ";
+            if (adj[i][j] == INF) cout << "0 ";
+            else cout << adj[i][j] << ' ';
         }
         cout << '\n';
     }
