01f6cc29b9b5644008a31f83c14b75ddf778f9ce
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-03-06 14:46:46 +0900

    [Silver IV] Title: 상근이의 여행, Time: 204 ms, Memory: 24244 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            while (m-- > 0) {
+                br.readLine();
+            }
+            sb.append(n - 1).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
