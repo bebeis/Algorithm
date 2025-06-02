d0d6fb92786575c7046d1533389285032b330bfd
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-06-02 18:25:24 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 532 ms, Memory: 58556 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -4,49 +4,39 @@ import java.util.*;
 public class Main {
 
     static int n;
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    static List<Integer> parent;
-    static boolean visited[];
+    static List<ArrayList<Integer>> adj = new ArrayList<>();
+    static int[] p = new int[100002];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
         }
-        parent = new ArrayList<>(Collections.nCopies(n + 1, 0));
-        visited = new boolean[n + 1];
-
         for (int i = 0; i < n - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
-
-        Deque<Integer> queue = new ArrayDeque<>();
-        visited[1] = true;
+        Queue<Integer> queue = new ArrayDeque<>();
         queue.offer(1);
 
         while (!queue.isEmpty()) {
             int cur = queue.poll();
 
-            graph.get(cur).stream()
-                .filter(nxt -> !visited[nxt])
-                .forEach(nxt -> {
-                    parent.set(nxt, cur);
-                    visited[nxt] = true;
-                    queue.offer(nxt);
-                });
+            for (int nxt : adj.get(cur)) {
+                if (nxt == p[cur]) continue;
+                p[nxt] = cur;
+                queue.offer(nxt);
+            }
         }
 
         StringBuilder sb = new StringBuilder();
-        parent.stream()
-            .skip(2)
-            .forEach(p -> sb.append(p).append('\n'));
-        
+        for (int i = 2; i <= n; i++) {
+            sb.append(p[i]).append('\n');
+        }
         System.out.print(sb);
     }
 }
