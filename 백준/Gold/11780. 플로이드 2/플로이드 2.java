b6ee7eab769f1edd825a42a1a9728288940576ec
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-06-16 20:55:13 +0900

    [Gold II] Title: 플로이드 2, Time: 584 ms, Memory: 54308 KB -BaekjoonHub

---
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int m;
+    final static int INF = 0x3f3f3f3f;
+    static int dist[][] = new int[101][101];
+    static int nxt[][] = new int[101][101];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], 1, n + 1, INF);
+            dist[i][i] = 0;
+        }
+
+        while (m-- > 0) {
+            String[] parts = br.readLine().split(" ");
+            int u = Integer.parseInt(parts[0]);
+            int v = Integer.parseInt(parts[1]);
+            int w = Integer.parseInt(parts[2]);
+            if (dist[u][v] > w) {
+                dist[u][v] = w;
+                nxt[u][v] = v;
+            }
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        nxt[i][j] = nxt[i][k];
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] == INF) sb.append(0);
+                else sb.append(dist[i][j]);
+                sb.append(' ');
+            }
+            sb.append('\n');
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] == INF || dist[i][j] == 0) {
+                    sb.append("0\n");
+                }
+                else {
+                    StringBuilder tempSB = new StringBuilder();
+                    int cnt = 1;
+                    int cur = i;
+                    while (cur != j) {
+                        tempSB.append(cur).append(' ');
+                        cur = nxt[cur][j];
+                        cnt++;
+                    }
+                    sb.append(cnt).append(' ').append(tempSB).append(j).append('\n');
+                }
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
