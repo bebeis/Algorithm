0022619556b22ff5b3160eb6df3085ce89bf50ad
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-06-27 20:35:12 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,22 @@
+-- 사원별 성과금 정보를 조회
+-- 사번, 성명, 평가 등급, 성과금을 조회하는 SQL문을 작성
+-- 평가등급의 컬럼명은 GRADE로, 성과금의 컬럼명은 BONUS로
+-- 결과는 사번 기준으로 오름차순 정렬
+
+SELECT E.EMP_NO, E.EMP_NAME, 
+CASE
+    WHEN AVG(SCORE) >= 96 THEN 'S'
+    WHEN AVG(SCORE) >= 90 THEN 'A'
+    WHEN AVG(SCORE) >= 80 THEN 'B'
+    ELSE 'C'
+END AS GRADE,
+CASE
+    WHEN AVG(SCORE) >= 96 THEN SAL * 0.2
+    WHEN AVG(SCORE) >= 90 THEN SAL * 0.15
+    WHEN AVG(SCORE) >= 80 THEN SAL * 0.1
+    ELSE 0
+END AS BONUS    
+FROM HR_EMPLOYEES E JOIN HR_GRADE G
+ON E.EMP_NO = G.EMP_NO
+GROUP BY E.EMP_NO, E.EMP_NAME
+ORDER BY E.EMP_NO ASC
