2cba7178883448881998f04514d5c25c10cb6a42
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-07-31 22:15:42 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1048 ms, Memory: 172568 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,37 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    
     public static void main(String[] args) throws IOException {
-        HashMap<Integer, Integer> cards = new HashMap<>(); 
-        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Integer temp;
-        for (int i = 0; i < n; i++) {
-            temp = Integer.parseInt(st.nextToken());
-            if (!cards.containsKey(temp)) {
-                cards.put(temp, 1);
-            }
-            else {
-                cards.put(temp, cards.get(temp) + 1);
-            }
+        Map<Integer, Integer> freq = new HashMap<>(N * 2);
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
         }
-        n = Integer.parseInt(br.readLine());
+
+        int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            temp = cards.get(Integer.parseInt(st.nextToken()));
-            if (temp == null) {
-                sb.append("0 ");
-            }
-            else {
-                sb.append(temp + " ");
-            }
+        for (int i = 0; i < M; i++) {
+            int q = Integer.parseInt(st.nextToken());
+            sb.append(freq.getOrDefault(q, 0))
+              .append(' ');
         }
+
         System.out.print(sb);
     }
 }
