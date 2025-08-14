408cb85570b09907f3b6df0e110a4dc2884e2de9
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-08-14 17:58:31 +0900

    [Gold II] Title: 확장 게임, Time: 496 ms, Memory: 57360 KB -BaekjoonHub

--- 백준/Gold/16920. 확장 게임/확장 게임.java
+++ 백준/Gold/16920. 확장 게임/확장 게임.java
@@ -3,9 +3,7 @@ import java.util.*;
 
 public class Main {
 
-    static int n;
-    static int m;
-    static int p;
+    static int n, m, p;
     static char[][] board = new char[1002][1002];
     static int[] s = new int[10];
     static int[] castle = new int[10];
@@ -20,36 +18,20 @@ public class Main {
         p = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= p; i++) {
-            s[i] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 1; i <= p; i++) s[i] = Integer.parseInt(st.nextToken());
+
+        Queue<int[]>[] queues = new Queue[p + 1];
+        for (int i = 1; i <= p; i++) queues[i] = new ArrayDeque<>();
 
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
                 board[i][j] = line.charAt(j);
                 if (Character.isDigit(board[i][j])) {
-                    castle[board[i][j] - '0']++;
-                }
-            }
-        }
-
-        Queue<int[]>[] queues = new Queue[p + 1];
-        for (int i = 1; i <= p; i++) queues[i] = new ArrayDeque<>();
-    
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (Character.isDigit(board[i][j])) {
                     int player = board[i][j] - '0';
+                    castle[player]++;
                     queues[player].offer(new int[]{i, j});
                 }
-            }
-        }
-
-        boolean[][] visited = new boolean[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] != '.') visited[i][j] = true;
             }
         }
 
@@ -59,38 +41,28 @@ public class Main {
             for (int player = 1; player <= p; player++) {
                 if (queues[player].isEmpty()) continue;
 
-                Queue<int[]> curQueue = new ArrayDeque<>();
-                Queue<int[]> nextQueue = new ArrayDeque<>();
-                curQueue.addAll(queues[player]);
-                queues[player].clear();
+                int moves = s[player];
+                Queue<int[]> q = queues[player];
 
-                int moveLimit = s[player];
-                for (int move = 0; move < moveLimit && !curQueue.isEmpty(); move++) {
-                    int size = curQueue.size();
-                    for (int k = 0; k < size; k++) {
-                        int[] pos = curQueue.poll();
-                        int x = pos[0], y = pos[1];
+                for (int move = 0; move < moves && !q.isEmpty(); move++) {
+                    int size = q.size();
+                    for (int i = 0; i < size; i++) {
+                        int[] cur = q.poll();
+                        int x = cur[0], y = cur[1];
 
                         for (int dir = 0; dir < 4; dir++) {
                             int nx = x + dx[dir];
                             int ny = y + dy[dir];
 
                             if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                            if (visited[nx][ny] || board[nx][ny] != '.') continue;
+                            if (board[nx][ny] != '.') continue;
 
-                            visited[nx][ny] = true;
                             board[nx][ny] = (char)(player + '0');
                             castle[player]++;
-                            nextQueue.offer(new int[]{nx, ny});
+                            q.offer(new int[]{nx, ny});
+                            anyMove = true;
                         }
                     }
-                    curQueue.addAll(nextQueue);
-                    nextQueue.clear();
-                }
-
-                if (!curQueue.isEmpty()) {
-                    queues[player].addAll(curQueue);
-                    anyMove = true;
                 }
             }
         } while (anyMove);
