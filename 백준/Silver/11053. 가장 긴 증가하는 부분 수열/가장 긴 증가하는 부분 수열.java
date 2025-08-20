44f0db1829a0272db64b56e72e699adafbded4cd
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-08-20 15:54:48 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,29 +1,36 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * 구하고자 하는 것 : 증가수열의 최대 길이
+ * 테이블에 기록할 수 있는 방법 후보
+ * 방법 1. key: I번째 포함 value : 길이 -- 11055에서 O(n^2)으로 풀었던 풀이와 동일
+ * 방법 2. key: I번째 까지 value : 최대 길이
+ * 방법 3. key: 길이      value : 마지막 원소가 위치하는 곳
+ */
+
 public class Main {
 
     static int n;
-    /**
-     * 주의할 점!!!
-     * 우리가 구하려고 하는거는 n에서의 증가하는 부분 수열의 최대 길이이 아니고, 이 수열 전체에서의 최대 길이
-     * i번째를 포함했다면, arr[i]보다 작은 arr[k] 여러 개를 찾은 뒤, 그 중 최대를 찾는다.
-     */
-    static int[] dp = new int[1002];
-    static int[] arr = new int[1002];
+    static int[] a = new int[1002];
+    static int[] d = new int[1002];
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] arghs) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            int localMax = 0;
-            for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i]) localMax = Math.max(localMax, dp[j]);
-            }
-            dp[i] = localMax + 1;
+            a[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.print(Arrays.stream(dp, 0, n).max().getAsInt());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (a[j] < a[i]) d[i] = Math.max(d[i], d[j]);
+            }
+            d[i] += 1;
+        }
+
+        System.out.print(Arrays.stream(d, 0, n).max().getAsInt());
     }
 }
