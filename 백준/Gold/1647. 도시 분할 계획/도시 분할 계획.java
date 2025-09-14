4efa43539526a607b874002f1029fd939f299bae
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-09-14 15:33:17 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1472 ms, Memory: 330968 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,75 +1,86 @@
 import java.io.*;
 import java.util.*;
 
-// 탈출 조건이 n - 1 -> n - 2로 바꾸는 것까지는 맞음.
-// n - 2로 바꾸면, 노드가 2개인 경우 반띵 나누면 경로가 없어도됨.
-// 먼저 ans를 더하고 if n - 2 == 0을 체크하기 때문에 n = 2에서 엣지 케이스 통과가 안됨
-// 엣지 케이스 놓쳐서 틀림
+/**
+ * n개의 노드, m개의 엣지 (양방향)
+ * 엣지를 유지하는 유지비 존재.
+ * 노드들은 어떻게든 연결되어 있음
+ */
 
+/**
+ * 목표: 두 개의 연결 요소로 분리하고, 엣지 유지비를 최소로 하게 만들자.
+ * - 분리된 두 마을 사이의 엣지 제거
+ * - 마을 내 불필요한 엣지 제거
+ */
+
+/**
+ * sol) 주어진 엣지 정보를 바탕으로 MST를 만들고, 가중치가 가장 큰 엣지를 제거
+ */
 public class Main {
 
-    static int n, m;
-    static int p[] = new int[100002];
-    static List<Edge> edges = new ArrayList<>();
+    static int n;
+    static int m;
+    static int[] p = new int[100002];
 
     public static int find(int x) {
         if (p[x] < 0) return x;
         return p[x] = find(p[x]);
     }
 
-    public static boolean isDiffGroup(int u, int v) {
-        u = find(u);
-        v = find(v);
+    public static boolean union(int x, int y) {
+        x = find(x);
+        y = find(y);
 
-        if (u == v) return false;
-        if (p[u] == p[v]) p[u]--;
-        if (p[u] < p[v]) p[v] = u;
-        else p[u] = v;
+        if (x == y) return false;
+        p[x] = y;
         return true;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        for (int i = 0; i <= n; i++) p[i] = -1;
+        Arrays.fill(p, 1, n + 1, -1);
 
+        List<Edge> edges = new ArrayList<>();
         while (m-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            edges.add(new Edge(a, b, c));
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            edges.add(new Edge(u, v, w));
         }
 
-        Collections.sort(edges);
-        int ans = 0, cnt = 0;
+        Collections.sort(edges, (e1, e2) -> e1.w - e2.w);
 
+        int cnt = 0;
+        int result = 0;
         for (Edge edge : edges) {
-            if (!isDiffGroup(edge.u, edge.v)) continue;
-            ans += edge.cost;
-            cnt++;
-            if (cnt == n - 2) break;
+            if (union(edge.s, edge.e)) {
+                result += edge.w;
+                cnt++;
+            }
+
+            if (cnt == n - 1) {
+                result -= edge.w;
+                break;
+            }
         }
 
-        if (n == 2) ans = 0;
-
-        System.out.print(ans);
+        System.out.print(result);
     }
 
-    static class Edge implements Comparable<Edge> {
-        int u, v, cost;
-        public Edge(int a, int b, int c) {
-            u = a;
-            v = b;
-            cost = c;
-        }
+    static class Edge {
+        int s;
+        int e;
+        int w;
 
-        @Override
-        public int compareTo(Edge e) {
-            return cost < e.cost ? -1 : (cost == e.cost ? 0 : 1);
+        public Edge(int s, int e, int w) {
+            this.s = s;
+            this.e = e;
+            this.w = w;
         }
     }
 }
