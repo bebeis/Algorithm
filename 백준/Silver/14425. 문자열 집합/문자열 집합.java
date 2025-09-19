630bf1a611c1d4dec2139327b4533a5e646a9a73
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-09-19 16:15:17 +0900

    [Silver IV] Title: 문자열 집합, Time: 1388 ms, Memory: 882216 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,25 +1,64 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+/**
+ * 트라이 자료구조
+ */
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        HashSet<String> words = new HashSet<>(); 
-        int count = 0;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < n; i++) {
-            words.add(br.readLine());
-        }
-        for (int i = 0; i < m; i++) {
-            if (words.contains(br.readLine())) {
-                count++;
+
+    final static int ROOT = 1;
+    final static int MX = 10000 * 500 + 5;
+
+    static int unused = 2;
+    static int[][] nxt = new int[MX][26];
+    static boolean[] chk = new boolean[MX];
+
+    public static void insert(String s) {
+        int cur = ROOT;
+        for (int i = 0; i < s.length(); i++) {
+            if (nxt[cur][ctoi(s.charAt(i))] == -1) {
+                nxt[cur][ctoi(s.charAt(i))] = unused++;
             }
+            cur = nxt[cur][ctoi(s.charAt(i))];
         }
-        System.out.print(count);
+        chk[cur] = true;
+    }
+
+    public static boolean find(String s) {
+        int cur = ROOT;
+        for (int i = 0; i < s.length(); i++) {
+            if (nxt[cur][ctoi(s.charAt(i))] == -1) {
+                return false;
+            }
+            cur = nxt[cur][ctoi(s.charAt(i))];
+        }
+        return chk[cur];
+    }
+
+    public static int ctoi(char c) {
+        return c - 'a';
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] parts = br.readLine().split(" ");
+        int n = Integer.parseInt(parts[0]);
+        int m = Integer.parseInt(parts[1]);
+        for (int i = 0; i < MX; i++) {
+            Arrays.fill(nxt[i], 0, 26, -1);
+        }
+
+        while (n-- > 0) {
+            insert(br.readLine());
+        }
+
+        int cnt = 0;
+        while (m-- > 0) {
+            if (find(br.readLine())) cnt++;
+        }
+
+        System.out.print(cnt);
+
     }
 }
