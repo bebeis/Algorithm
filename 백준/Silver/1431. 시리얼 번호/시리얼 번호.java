e851ab9af1f6023158ae02b52af97e9a34df8a77
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-12-03 19:09:54 +0900

    [Silver III] Title: 시리얼 번호, Time: 116 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
@@ -1,28 +1,46 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<String> words = new ArrayList<>();
-        while (n-- > 0) {
-            words.add(br.readLine());
+        List<String> inputs = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            inputs.add(br.readLine());
         }
-        Collections.sort(words, (s1, s2) -> {
-            if (s1.length() == s2.length()) {
-                int sum1 = s1.chars().filter(Character::isDigit).map(c -> (c - '0')).sum();
-                int sum2 = s2.chars().filter(Character::isDigit).map(c -> (c - '0')).sum();
-                if (sum1 != sum2) {
-                    return sum1 - sum2;
-                } else {
-                    return s1.compareTo(s2);
-                }
-            } else {
+
+        Collections.sort(inputs, (s1, s2) -> {
+            // 1.
+            if (s1.length() != s2.length()) {
                 return s1.length() - s2.length();
             }
+
+            // 2. 
+            int s1Val = getVal(s1);
+            int s2Val = getVal(s2);
+            if (s1Val != s2Val) {
+                return s1Val - s2Val;
+            }
+
+            // 3.
+            return s1.compareTo(s2);
         });
-        words.forEach(System.out::println);
+
+        StringBuilder sb = new StringBuilder();
+        inputs.forEach(s -> sb.append(s).append('\n'));
+        System.out.print(sb);
+    }
+
+    private static int getVal(String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isDigit(c)) {
+                sum += (c - '0');
+            }
+        }
+        return sum;
     }
 }
