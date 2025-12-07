49b6012ff3198485b24fa69b24311511b3320f43
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-12-08 00:50:40 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1368 ms, Memory: 124132 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,26 +1,63 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * 문제: 주어진 숫자를 몇 개 가지고 있는지 구하기
+ * 조건: n <= 500,000, m <= 500,000
+ * 원소 하나당 O(logn)으로 처리해야함 -> 정렬된 상태에서 검색 (트리, 이분 탐색)
+ */
+
 public class Main {
+
+    static int n;
+    static int[] arr = new int[500002];
+
+    static int m;
+
+    public static int upperBound(int target) {
+        int lo = -1;
+        int hi = n;
+
+        while (lo + 1 < hi) {
+            int mid = (lo + hi) / 2;
+            if (arr[mid] <= target) lo = mid;
+            else hi = mid;
+        }
+
+        return hi;
+    }
+
+    // 1 2 3 4 5 6
+    // target = 7
+
+    public static int lowerBound(int target) {
+        int lo = -1;
+        int hi = n;
+
+        while (lo + 1 < hi) {
+            int mid = (lo + hi) / 2;
+            if (arr[mid] < target) lo = mid;
+            else hi = mid;
+        }
+
+        return hi;
+    }
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int N = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Map<Integer, Integer> freq = new HashMap<>(N * 2);
-        for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(st.nextToken());
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.parallelSort(arr, 0, n);
 
-        int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int q = Integer.parseInt(st.nextToken());
-            sb.append(freq.getOrDefault(q, 0))
-              .append(' ');
+        while (m-- > 0) {
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(upperBound(target) - lowerBound(target)).append(' ');
         }
 
         System.out.print(sb);
