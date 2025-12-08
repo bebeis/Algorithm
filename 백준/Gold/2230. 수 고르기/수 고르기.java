8ba360726de7c178d60fb1a690374f9c9c26dc1f
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-12-08 17:29:39 +0900

    [Gold V] Title: 수 고르기, Time: 388 ms, Memory: 28728 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -2,34 +2,41 @@ import java.io.*;
 import java.util.*;
 
 /**
- * O(nlogn) 이하로 풀이!
+ * 문제: 두 수의 차이의 lowerbound 구하기
+ * 입력 조건: n <= 100,000 -> O(nlogn)
+ * - 브루트 포스 불가능. 즉, 모든 쌍의 차를 구하면 안 됨
+ * - 순서를 유지할 필요 없음 -> 정렬
  */
-
 public class Main {
 
+    static int n;
+    static int m;
     static int[] a = new int[100002];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] parts = br.readLine().split(" ");
-        int n = Integer.parseInt(parts[0]);
-        int m = Integer.parseInt(parts[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(a, 0, n);
-        
+
+        Arrays.parallelSort(a, 0, n);
+        int head = 0, tail = 0;
         int result = Integer.MAX_VALUE;
-        int st = 0, ed = 0;
-        while (st <= n && ed <= n) {
-            int diff = a[st] - a[ed];
-            if (diff >= m) {
-                result = Math.min(result, diff);
-                ed++;
+        while (head < n && tail < n) {
+            int diff = a[head] - a[tail];
+            if (diff < m) {
+                head++;
             } else {
-                st++;
+                result = Math.min(result, diff);
+                if (head == tail) head++;
+                else tail++;
             }
         }
+
         System.out.print(result);
     }
 }
