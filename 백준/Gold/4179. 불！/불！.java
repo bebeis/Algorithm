3b98c4484039be9ede690e2689ce76baf6ff0e64
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-12-14 15:15:33 +0900

    [Gold III] Title: 불!, Time: 504 ms, Memory: 54576 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,87 +1,87 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
+/**
+ * 문제: 미로 탈출
+ * 조건: 장애물의 퍼짐
+ */
+
+class Main {
+
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
     static int n;
     static int m;
     static char[][] board = new char[1002][1002];
-    static int[][] fireDist = new int[1002][1002];
-    static int[][] personDist = new int[1002][1002];
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    static int[][] fd = new int[1002][1002];
+    static int[][] jd = new int[1002][1002];
+    static Queue<int[]> FQ = new ArrayDeque<>();
+    static Queue<int[]> JQ = new ArrayDeque<>();
 
-    // 불에 타기전에 탈 수 있는지 여부
-    // 얼마나 빨리 탈출할 수 있는지를 결정
-    // 미로는 가장자리에서 탈출 가능
-    // 벽 통과 불과
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] strings = br.readLine().split(" ");
-        n = Integer.parseInt(strings[0]);
-        m = Integer.parseInt(strings[1]);
-        Queue<Pair> fireQueue = new ArrayDeque<>();
-        Queue<Pair> personQueue = new ArrayDeque<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
+        
         for (int i = 0; i < n; i++) {
             String input = br.readLine();
             for (int j = 0; j < m; j++) {
                 board[i][j] = input.charAt(j);
-                fireDist[i][j] = -1;
-                personDist[i][j] = -1;
+                fd[i][j] = -1;
+                jd[i][j] = -1;
                 if (board[i][j] == 'F') {
-                    fireQueue.offer(new Pair(i, j));
-                    fireDist[i][j] = 0;
+                    FQ.offer(new int[]{i, j});
+                    fd[i][j] = 0;
                 }
                 else if (board[i][j] == 'J') {
-                    personQueue.offer(new Pair(i, j));
-                    personDist[i][j] = 0;
+                    JQ.offer(new int[]{i, j});
+                    jd[i][j] = 0;
                 }
             }
         }
-
-        // 불 이동 거리 
-        while (!fireQueue.isEmpty()) {
-            var cur = fireQueue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (board[nx][ny] == '#' || fireDist[nx][ny] != -1) continue;
-                fireDist[nx][ny] = fireDist[cur.x][cur.y] + 1;
-                fireQueue.offer(new Pair(nx, ny));
-            }
-        }
-
-        // 사람 이동
-        while (!personQueue.isEmpty()) {
-            var cur = personQueue.poll();
-            if (cur.x == 0 || cur.x == n - 1 || cur.y == 0 || cur.y == m - 1) {
-                System.out.print(personDist[cur.x][cur.y] + 1);
-                return;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (board[nx][ny] == '#' || personDist[nx][ny] != -1 || (fireDist[nx][ny] != -1 && personDist[cur.x][cur.y] + 1 >= fireDist[nx][ny])) continue;
-                personDist[nx][ny] = personDist[cur.x][cur.y] + 1;
-                personQueue.offer(new Pair(nx, ny));
-            }
-        }
-        System.out.print("IMPOSSIBLE");
+        int result = solve();
+        System.out.print(result != -1 ? result : "IMPOSSIBLE");
     }
 
-    static class Pair {
-        int x; int y;
-        public Pair(int xx, int yy) {
-            x = xx;
-            y = yy;
+    private static int solve() {
+        fireBFS();
+        while (!JQ.isEmpty()) {
+            var cur = JQ.poll();
+            int nextDist = jd[cur[0]][cur[1]] + 1;
+            if (cur[0] == 0 || cur[0] == n - 1 || cur[1] == 0 || cur[1] == m - 1) {
+                return nextDist;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (board[nx][ny] == '#' || jd[nx][ny] != -1) continue;
+                if (fd[nx][ny] != -1 && nextDist >= fd[nx][ny]) continue;
+                jd[nx][ny] = nextDist;
+                JQ.offer(new int[]{nx, ny});
+            }
+        }
+        return -1;
+    }
+
+    private static void fireBFS() {
+        while (!FQ.isEmpty()) {
+            var cur = FQ.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (board[nx][ny] == '#' || fd[nx][ny] != -1) continue;
+                fd[nx][ny] = fd[cur[0]][cur[1]] + 1;
+                FQ.offer(new int[]{nx, ny});
+            }
         }
     }
 }
