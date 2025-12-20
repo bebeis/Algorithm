c8e2572e7c62daf35723dbfe1de02c58a0855bed
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-12-20 21:47:51 +0900

    [Gold IV] Title: 불, Time: 676 ms, Memory: 87136 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -1,90 +1,107 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    
-    static int dx[] = {1, 0, -1, 0};
-    static int dy[] = {0, 1, 0, -1};
+class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int m;
+    static int n;
+    static char[][] board;
+    static int[][] fd;
+    static int[][] pd;
+    static Queue<int[]> fq;
+    static Queue<int[]> pq;
+    final static int[] dx = { 1, 0, -1, 0 };
+    final static int[] dy = { 0, 1, 0, -1 };
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
         while (t-- > 0) {
-            char[][] board = new char[1002][1002];
-            int fireDist[][] = new int[1002][1002];
-            int personDist[][] = new int[1002][1002]; 
-            Queue<Pair> queue = new ArrayDeque<>();
-
-            String[] parts = br.readLine().split(" ");
-            Pair start = null;
-            int w = Integer.parseInt(parts[0]);
-            int h = Integer.parseInt(parts[1]);
-            for (int i = 0; i < h; i++) {
-                String input = br.readLine();
-                for (int j = 0; j < w; j++) {
-                    board[i][j] = input.charAt(j);
-                    fireDist[i][j] = -1;
-                    personDist[i][j] = -1;
-                    
-                    if (board[i][j] == '@') {
-                        start = new Pair(i, j);
-                        personDist[i][j] = 0;
-                    } else if (board[i][j] == '*') {
-                        queue.offer(new Pair(i, j));
-                        fireDist[i][j] = 0;
-                    }
-                }
-            }
-            // 불 도착 시간 측정
-            while (!queue.isEmpty()) {
-                var cur = queue.poll();
-
-                for (int i = 0; i < 4; i++) {
-                    int nx = cur.x + dx[i];
-                    int ny = cur.y + dy[i];
-
-                    if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
-                    if (board[nx][ny] == '#' || fireDist[nx][ny] != -1) continue;
-                    fireDist[nx][ny] = fireDist[cur.x][cur.y] + 1;
-                    queue.offer(new Pair(nx, ny));
-                }
-            }
-
-            String result = "IMPOSSIBLE";
-            queue.offer(start);
-            while (!queue.isEmpty()) {
-                var cur = queue.poll();
-
-                if (cur.x == h - 1 || cur.y == w - 1 || cur.x == 0 || cur.y == 0) {
-                    result = String.valueOf(personDist[cur.x][cur.y] + 1);
-                    break;
-                }
-
-                for (int i = 0; i < 4; i++) {
-                    int nx = cur.x + dx[i];
-                    int ny = cur.y + dy[i];
-
-                    if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
-                    if (board[nx][ny] == '#' || personDist[nx][ny] != -1) continue;
-                    if (fireDist[nx][ny] != -1 && fireDist[nx][ny] <= personDist[cur.x][cur.y] + 1) continue;
-                    personDist[nx][ny] = personDist[cur.x][cur.y] + 1;
-                    queue.offer(new Pair(nx, ny));
-                }
-            }
-            sb.append(result).append('\n');
+            testcase();
         }
         System.out.print(sb);
     }
 
-    static class Pair {
-        int x;
-        int y;
+    private static void testcase() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        board = new char[n][m];
+        fd = new int[n][m];
+        pd = new int[n][m];
 
-        public Pair(int xx, int yy) {
-            x = xx;
-            y = yy;
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < m; j++) {
+                board[i][j] = input.charAt(j);
+                fd[i][j] = -1;
+                pd[i][j] = -1;
+            }
         }
+        sb.append(solve()).append('\n');
+    }
+
+    private static String solve() {
+        fq = new ArrayDeque<>();
+        pq = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == '@') {
+                    pq.add(new int[]{i, j});
+                    pd[i][j] = 0;
+                } else if (board[i][j] == '*') {
+                    fq.add(new int[]{i, j});
+                    fd[i][j] = 0;
+                }
+            }
+        }
+        fireBFS();
+        int result = personBFS();
+
+        if (result == -1) return "IMPOSSIBLE";
+        return String.valueOf(result);
+    }
+
+    private static void fireBFS() {
+        while (!fq.isEmpty()) {
+            var cp = fq.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cp[0] + dx[i];
+                int ny = cp[1] + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (board[nx][ny] == '#') continue;
+                if (fd[nx][ny] != -1) continue;
+                fd[nx][ny] = fd[cp[0]][cp[1]] + 1;
+                fq.offer(new int[]{nx, ny});
+            }
+        }
+    }
+
+    private static int personBFS() {
+        while (!pq.isEmpty()) {
+            var cp = pq.poll();
+            int nd = pd[cp[0]][cp[1]] + 1;
+            if (cp[0] == 0 || cp[0] == n - 1 || cp[1] == 0 || cp[1] == m - 1) {
+                return nd;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cp[0] + dx[i];
+                int ny = cp[1] + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (board[nx][ny] == '#') continue;
+                if (fd[nx][ny] != -1 && fd[nx][ny] <= nd) continue;
+                if (pd[nx][ny] != -1) continue;
+
+                pd[nx][ny] = nd;
+                pq.offer(new int[]{nx, ny});
+            }
+        }
+
+        return -1;
     }
 }
