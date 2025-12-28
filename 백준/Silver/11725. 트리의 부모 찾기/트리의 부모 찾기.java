2be25d10feb22fe9f369ac273efd838861ddd944
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2025-12-28 23:52:12 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 552 ms, Memory: 58452 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,10 +1,9 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
 
-    static int n;
-    static List<ArrayList<Integer>> adj = new ArrayList<>();
+    static List<List<Integer>> adj = new ArrayList<>();
     static int[] p = new int[100002];
 
     public static void main(String[] args) throws IOException {
@@ -13,18 +12,20 @@ public class Main {
         for (int i = 0; i <= n; i++) {
             adj.add(new ArrayList<>());
         }
+
         for (int i = 0; i < n - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
+
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
+
         Queue<Integer> queue = new ArrayDeque<>();
         queue.offer(1);
-
         while (!queue.isEmpty()) {
-            int cur = queue.poll();
+            var cur = queue.poll();
 
             for (int nxt : adj.get(cur)) {
                 if (nxt == p[cur]) continue;
@@ -34,9 +35,8 @@ public class Main {
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 2; i <= n; i++) {
-            sb.append(p[i]).append('\n');
-        }
+        Arrays.stream(p, 2, n + 1)
+            .forEach(x -> sb.append(x).append('\n'));
         System.out.print(sb);
     }
 }
