f4d09fd6a980c3e178313afdbf0a8762a593beea
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-02-05 00:29:39 +0900

    [Silver I] Title: 기타콘서트, Time: 112 ms, Memory: 21316 KB -BaekjoonHub

--- 백준/Silver/1497. 기타콘서트/기타콘서트.kt
+++ 백준/Silver/1497. 기타콘서트/기타콘서트.kt
@@ -1,40 +1,45 @@
-const val MAX = 12
-
 fun main() = with(System.`in`.bufferedReader()) {
     val (n, m) = readLine().split(" ").map { it.toInt() }
-    val arr = Array(n) {
-        val (_, status) = readLine().split(" ")
-        BooleanArray(m) { idx -> status[idx] == 'Y' }
-    }
-
-    val possible = BooleanArray(m)
-    for (i in 0 until n) {
-        for (j in 0 until m) {
-            if (arr[i][j]) possible[j] = true
+    infix fun Long.on(i: Int) = this or (1L shl (i - 1))
+    infix fun Long.check(i: Int) = if (this and (1L shl (i - 1)) >= 1L) 1 else 0
+    infix fun Long.union(other: Long): Long {
+        var tmp = this
+        for (i in 1..m) {
+            if (other.check(i) == 1) {
+                tmp = tmp.on(i)
+            }
         }
+        return tmp
     }
-    val maxSongCount = possible.count { it }
 
+    val arr = LongArray(n) {
+        val (_, status) = readLine().split(" ")
+        var temp = 0L
+        for ((idx, element) in status.toCharArray().withIndex()) {
+            if (element == 'Y') temp = temp.on(idx + 1)
+        }
+        temp
+    }
+
+    val unioned = arr.fold(0L, Long::union)
+    val maxSongCount = unioned.countOneBits()
     if (maxSongCount == 0) {
         print(-1)
         return@with
     }
 
-    var songs = BooleanArray(m)
-    var min = MAX
+    var songs = 0L
+    var min = 12
 
-    fun bt(arrIdx: Int, cnt: Int) {
-        val songCount = songs.count { it }
-        if (songCount == maxSongCount) {
+    fun bt(arrIdx: Int, cnt: Int) { 
+        if (songs.countOneBits() == maxSongCount) {
             min = minOf(cnt, min)
             return
         }
 
         for (i in arrIdx until n) {
-            val temp = songs.copyOf()
-            for ((idx, element) in arr[i].withIndex()) {
-                if (element) songs[idx] = true
-            }
+            val temp = songs
+            songs = songs.union(arr[i])
             bt(i + 1, cnt + 1)
             songs = temp
         }
