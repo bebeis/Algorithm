52808067ce5254bddff07b3c4d57ec5b88b64ce7
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-02-07 18:29:39 +0900

    [Silver II] Title: 에디터, Time: 424 ms, Memory: 64104 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.kt
+++ 백준/Silver/1406. 에디터/에디터.kt
@@ -1,18 +1,18 @@
 import java.util.*
 fun main() = with(System.`in`.bufferedReader()) {
-    val leftList = LinkedList(readLine().toList())
-    val rightList = LinkedList<Char>()
+    val list = LinkedList(readLine().toList())
+    val iter = list.listIterator(list.size)
 
     repeat(readLine().toInt()) {
         readLine().run {
             when (this[0]) {
-                'P' -> leftList.addLast(this[2])
-                'L' -> leftList.removeLastOrNull() ?.let { rightList.addFirst(it) }
-                'D' -> rightList.removeFirstOrNull() ?.let { leftList.addLast(it) }
-                'B' -> leftList.removeLastOrNull()
+                'P' -> iter.add(this[2])
+                'L' -> if (iter.hasPrevious()) iter.previous()
+                'D' -> if (iter.hasNext()) iter.next()
+                'B' -> if (iter.hasPrevious()) { iter.previous(); iter.remove() }
             }
         }
     }
 
-    print(leftList.joinToString("") + rightList.joinToString(""))
+    print(list.joinToString(""))
 }
