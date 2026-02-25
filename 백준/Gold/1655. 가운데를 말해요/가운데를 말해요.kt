f9f439dadc3c7548b26bc416dbc736e70e2882e9
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-02-25 22:21:48 +0900

    [Gold II] Title: 가운데를 말해요, Time: 360 ms, Memory: 31068 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.kt
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.kt
@@ -3,43 +3,18 @@ import java.util.*
 fun main() = with(System.`in`.bufferedReader()) {
     val sb = StringBuilder()
     val minHeap = PriorityQueue<Int>()
-    val maxHeap = PriorityQueue<Int>(compareByDescending<Int>{it})
+    val maxHeap = PriorityQueue<Int>(reverseOrder())
+
     repeat(readLine().toInt()) {
         val cur = readLine().toInt()
-        if (maxHeap.isEmpty()) {
-            maxHeap.offer(cur)
-            sb.append("$cur\n")
-        } else if (minHeap.isEmpty()) {
-            val midUpperOne = maxHeap.peek()
-            if (midUpperOne > cur) {
-                maxHeap.poll()
-                minHeap.offer(midUpperOne)
-                maxHeap.offer(cur)
-            } else {
-                minHeap.offer(cur)
-            }
-            val mid = maxHeap.peek()
-            sb.append("$mid\n")
-        } else {
-            val originMid = maxHeap.peek()
-            if (cur > originMid) {
-                if (minHeap.size < maxHeap.size) {
-                    minHeap.offer(cur)
-                } else {
-                    minHeap.offer(cur)
-                    maxHeap.offer(minHeap.poll())
-                }
-            } else {
-                if (minHeap.size < maxHeap.size) {
-                    maxHeap.offer(cur)
-                    minHeap.offer(maxHeap.poll())
-                } else {
-                    maxHeap.offer(cur)
-                }
-            }
-            val mid = maxHeap.peek()
-            sb.append("$mid\n")
-        }
+
+        if (maxHeap.isEmpty() || cur <= maxHeap.peek()) maxHeap.offer(cur)
+        else minHeap.offer(cur)
+
+        if (maxHeap.size > minHeap.size + 1) minHeap.offer(maxHeap.poll())
+        else if (minHeap.size > maxHeap.size) maxHeap.offer(minHeap.poll())
+
+        sb.appendLine(maxHeap.peek())
     }
     print(sb)
 }
