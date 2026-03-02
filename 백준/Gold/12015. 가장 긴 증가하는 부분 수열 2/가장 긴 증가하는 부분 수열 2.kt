986309d8695c083d4c4e573c2a27eaec73c568e0
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-03-02 21:40:27 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 464 ms, Memory: 129632 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.kt
@@ -0,0 +1,45 @@
+import java.util.*
+
+// d[x]: x가 선택되었을 때, 가장 긴 수열의 길이
+// d[x]: d[k] + 1 
+// k는, arr[x] 보다 arr[k]가 작지만 d[k]가 가장 큰 것) -> 기존엔 O(n)이 었지만, 개선해야 함
+// arr[j] < arr[i] 인 것 중에서 최대를 찾는다.
+// 오름차순이 유지된다 -> 모노톤 스택을 생각해보자.
+// 값을 오름차순으로 저장하고, 인덱스를 길이로 사용한다 -> log 스케일로 탐색이 가능해짐
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val arr = StringTokenizer(readLine()).let { st -> IntArray(n) { st.nextToken().toInt() } }
+    val d = IntArray(n)
+    val lv = IntArray(n + 1) // key: length, value: 해당 길이에서의 값
+
+    d[0] = 1
+    lv[1] = arr[0]
+    var maxLength = 1
+
+    fun lowerbound(target: Int): Int {
+        var low = -1
+        var high = maxLength + 1
+
+        while (low + 1 < high) {
+            val mid = (low + high) / 2
+            if (lv[mid] >= target) high = mid
+            else low = mid
+        }
+
+        return high
+    }
+
+    for (i in 1 until n) {
+        if (arr[i] > lv[maxLength]) {
+            d[i] = ++maxLength
+            lv[maxLength] = arr[i]
+        } else {
+            val idx = lowerbound(arr[i])
+            d[i] = idx
+            lv[idx] = arr[i]
+        }
+    }
+
+    print(maxLength)
+}
