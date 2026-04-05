34027e834bc56363cd48f9ed03807e8117bc2ec7
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-04-05 16:05:57 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 136 ms, Memory: 15484 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -2,21 +2,18 @@
 using namespace std;
 
 int board[1001][1001];
-int dist[2][1001][1001]; // 벽을 뚫은 횟수, 좌표
+int dist[2][1001][1001];
 
 const int dx[] = {1, 0, -1, 0};
 const int dy[] = {0, 1, 0, -1};
-const int MAX_DRILL = 1;
 
 int main(void) {
     int n, m;
     scanf("%d %d", &n, &m);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
             scanf("%1d", &board[i][j]);
-        }
-    }
 
     queue<tuple<int, int, int>> q;
     q.push({0, 0, 0});
@@ -25,6 +22,7 @@ int main(void) {
     while (!q.empty()) {
         auto [cnt, cx, cy] = q.front();
         q.pop();
+
         if (cx == n - 1 && cy == m - 1) {
             printf("%d", dist[cnt][cx][cy]);
             return 0;
@@ -33,20 +31,13 @@ int main(void) {
         for (int i = 0; i < 4; i++) {
             int nx = cx + dx[i];
             int ny = cy + dy[i];
-
             if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
 
-            // nxt가 벽인 경우
-            if (board[nx][ny] == 1) {
-                if (cnt >= MAX_DRILL) continue;
-                if (dist[cnt + 1][nx][ny] != 0) continue;
-                dist[cnt + 1][nx][ny] = dist[cnt][cx][cy] + 1;
-                q.push({cnt + 1, nx, ny});
-            } else {
-                if (dist[cnt][nx][ny] != 0) continue;
-                dist[cnt][nx][ny] = dist[cnt][cx][cy] + 1;
-                q.push({cnt, nx, ny});
-            }
+            int ncnt = cnt + board[nx][ny];
+            if (ncnt > 1 || dist[ncnt][nx][ny]) continue;
+
+            dist[ncnt][nx][ny] = dist[cnt][cx][cy] + 1;
+            q.push({ncnt, nx, ny});
         }
     }
     printf("-1");
