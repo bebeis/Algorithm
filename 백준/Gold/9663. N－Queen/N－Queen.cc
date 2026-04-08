a509fcbcf5a10634f9c7ae7d34589959bbc2249a
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-04-08 23:01:11 +0900

    [Gold IV] Title: N-Queen, Time: 1612 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,30 +1,42 @@
+/**
+ * 하나의 열에 최대 하나의 퀸만 배치할 수 있음 -> 이걸 스택의 상태로 유지
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int visited[16][16];
 int n;
+bool horizon[17]; // x = a
+bool diagRight[32]; // x - y = a - b + n
+bool diagLeft[32]; // x + y = a + b
 int cnt = 0;
 
-void getWay(int row) {
-    if (row == n) {
+void bt(int b) {
+    if (b == n) {
         cnt++;
         return;
     }
-    for (int i = 0; i < n; i++) {
-        if (visited[row][i]) continue;
-        for (int j = row; j < n; j++) visited[j][i]++;
-        for (int j = row, diag = i; j < n && diag < n; j++, diag += 1) visited[j][diag]++;
-        for (int j = row, diag = i; j < n && diag >= 0; j++, diag -= 1) visited[j][diag]++;
-        getWay(row + 1);
-        for (int j = row; j < n; j++) visited[j][i]--;
-        for (int j = row, diag = i; j < n && diag < n; j++, diag += 1) visited[j][diag]--;
-        for (int j = row, diag = i; j < n && diag >= 0; j++, diag -= 1) visited[j][diag]--;
+
+    for (int a = 0; a < n; a++) {
+        if (horizon[a]) continue;
+        if (diagRight[a - b + n]) continue;
+        if (diagLeft[a + b]) continue;
+
+        horizon[a] = true;
+        diagRight[a - b + n] = true;
+        diagLeft[a + b] = true;
+        bt(b + 1);
+
+        horizon[a] = false;
+        diagRight[a - b + n] = false;
+        diagLeft[a + b] = false;
     }
 }
 
 int main(void) {
-    cin.tie(0)->sync_with_stdio(false);
+    cin.tie(0) -> sync_with_stdio(false);
     cin >> n;
-    getWay(0);
+
+    bt(0);
     cout << cnt;
 }
