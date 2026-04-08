9463a09fc4d123d4afa313c256c4aca1bbb21b97
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-04-08 22:40:29 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,26 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m;
 
+bool used[10];
 int arr[10];
-bool vis[10];
-int n, m; // 1~n 중 m개를 고른다
 
-void backtracking(int target) {
+void bt(int curLength) {
+    if (curLength == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
     for (int i = 1; i <= n; i++) {
-        if (vis[i]) continue;
-        arr[target] = i;
-        vis[i] = true;
-        if (target == m) {
-            for (int i = 1; i <= m; i++) cout << arr[i] << " ";
-            cout << '\n';
-        } else backtracking(target + 1);
-        vis[i] = false;
+        if (used[i]) continue;
+
+        used[i] = true;
+        arr[curLength] = i;
+        bt(curLength + 1);
+        used[i] = false;
     }
 }
 
 int main(void) {
-    cin.tie(0)->sync_with_stdio(false);
+    cin.tie(0) -> sync_with_stdio(false);
     cin >> n >> m;
-    backtracking(1);
+    bt(0);
 }
