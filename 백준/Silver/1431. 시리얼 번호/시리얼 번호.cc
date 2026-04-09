f5f4c5fc772047b41ebdb0ee36ca98c783364015
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-04-09 12:39:35 +0900

    [Silver III] Title: 시리얼 번호, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
@@ -1,37 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string arr[52];
-int n;
-
-bool cmp(string a, string b) {
-    if (a.length() < b.length()) return true;
-    else if (a.length() == b.length()) {
-        int aSum = 0;
-        int bSum = 0;
-        for (char c : a) {
-            if (isdigit(c)) aSum += (c - '0');
-        }
-        for (char c : b) {
-            if (isdigit(c)) bSum += (c - '0');
-        }
-        if (aSum < bSum) return true;
-        else if (aSum == bSum) {
-            return a < b;
-        }
-        else {
-            return false;
-        }
-    } else {
-        return false;
-    }
-    return false;
-}
-
 int main(void) {
-    cin.tie(0)->sync_with_stdio(false);
-    cin >> n;
-    for (int i = 0; i < n; i++) cin >> arr[i];
-    sort(arr, arr + n, cmp);
-    for (int i = 0; i < n; i++) cout << arr[i] << '\n';
+    cin.tie(0) -> sync_with_stdio(false);
+    int n; cin >> n;
+
+    vector<string> words;
+    for (int i = 0; i < n; i++) {
+        string s; cin >> s;
+        words.push_back(s);
+    }
+
+    sort(words.begin(), words.end(), [](const auto& s1, const auto& s2) {
+        if (s1.length() != s2.length()) return s1.length() < s2.length();
+        int sum1 = 0; int sum2 = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (isdigit(s1[i])) sum1 += (s1[i] - '0');
+            if (isdigit(s2[i])) sum2 += (s2[i] - '0');
+        }
+
+        if (sum1 != sum2) return sum1 < sum2;
+        return s1 < s2;
+    });
+
+    for (auto& word : words) {
+        cout << word << '\n';
+    }
 }
