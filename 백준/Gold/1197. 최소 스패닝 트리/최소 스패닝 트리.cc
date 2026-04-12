395efd6171cdb1ce994e0fcf59dd06040b448b71
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-04-12 13:08:18 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,40 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int parent[10001];
+int p[10002];
 vector<pair<int, pair<int, int>>> edges;
 
-// find
 int find(int x) {
-    if (x == parent[x]) return x;
-    else return parent[x] = find(parent[x]);
+    if (p[x] < 0) return x;
+    return p[x] = find(p[x]);
 }
-// union
-void merge(int a, int b) {
-    int x = find(a);
-    int y = find(b);
-    if (x == y) return;
-    parent[y] = x; // y -> x
+
+bool uni(int u, int v) {
+    u = find(u); v = find(v);
+
+    if (u == v) return false;
+
+    if (p[v] < p[u]) // y의 랭크가 x보다 큰 경우
+        swap(u, v); // x를 더 크게 만든다. 
+    if (p[u] == p[v]) p[u]--;
+    p[v] = u;
+    return true;
 }
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int v, e;
-    long long result = 0;
-    cin >> v >> e;
-    while (e--) {
-        int st, ed, weight;
-        cin >> st >> ed >> weight;
-        edges.push_back({weight, {st, ed}});
+    cin.tie(0) -> sync_with_stdio(false);
+    fill(p, p + 10002, -1);
+
+    int v, e; cin >> v >> e;
+    while (e-- > 0) {
+        int a, b, c; cin >> a >> b >> c;
+        edges.push_back({c, {a, b}});
     }
+
     sort(edges.begin(), edges.end());
-    for (int i = 1; i <= v; i++) parent[i] = i;
-    for (auto edge : edges) {
-        if (find(edge.second.first) != find(edge.second.second)) {
-            merge(edge.second.first, edge.second.second);
-            result += edge.first;
+
+    int sum = 0;
+    int cnt = 0;
+    for (auto [w, l] : edges) {
+        if (uni(l.first, l.second)) {
+            sum += w;
+            cnt++;
         }
+
+        if (cnt == v - 1) break;
     }
-    cout << result;
+
+    cout << sum;
 }
