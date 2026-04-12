3f7082195a3bd0b1ee1c0c52929cd0f2eb4cb2f3
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-04-12 12:31:39 +0900

    [Gold II] Title: 보석 도둑, Time: 228 ms, Memory: 18492 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,38 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 상덕이는 가방을 K개 가지고 있고, 각 가방에 담을 수 있는 최대 무게는 Ci이다
-// 상덕이가 훔칠 수 있는 보석의 최대 가격 구하기
-// 가방에는 보석을 1개만 넣을 수 있다.
-// O(nlogn), o(nlogc), o(clogc)이하로 풀어야 함
-// 처음에 문제 푸는 아이디어는 옳았다.
-// 시간초과 난 이유 : while (idx < k && bag_object[idx] != 0) idx++;인 듯한데
-// 값을 대입한 인덱스 자체를 리스트에서 삭제 해주면 굉장히 빠르게 처리가 가능하다.
-// --> bag_object 대신 multiset 사용
-
-int bag[300001];
+pair<int, int> jewels[300002]; // 가치, 무게
+multiset<int> bags;
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int n, k, m, v;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, less<pair<int, int>>> pq;
-    cin >> n >> k;
+    cin.tie(0) -> sync_with_stdio(false);
+    
+    int n, k; cin >> n >> k;
     for (int i = 0; i < n; i++) {
-        cin >> m >> v;
-        pq.push({v, m});
+        cin >> jewels[i].second >> jewels[i].first;
     }
-    for (int i = 0; i < k; i++) cin >> bag[i];
-    sort(bag, bag + k);
-    multiset<int> available_bags(bag, bag + k);
+
+    for (int i = 0; i < k; i++) {
+        int w; cin >> w;
+        bags.insert(w);
+    }
+
+    sort(jewels, jewels + n);
     long long sum = 0;
-    while (!pq.empty()) {
-        auto [price, weight] = pq.top(); pq.pop();
-        auto it = available_bags.lower_bound(weight);
-        if (it != available_bags.end()) {
-            sum += price;
-            available_bags.erase(it);
-        }
+
+    for (int i = n - 1; i >= 0; i--) {
+        auto targetIt = bags.lower_bound(jewels[i].second);
+        if (targetIt == bags.end()) continue;
+        sum += jewels[i].first;
+        bags.erase(targetIt);
     }
     cout << sum;
 }
