cd1482ecd3c5475fd948d39e0c30f3de7fdb85de
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-04-12 10:30:34 +0900

    [Silver II] Title: 주식, Time: 576 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.cc
+++ 백준/Silver/11501. 주식/주식.cc
@@ -1,24 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-// 고점에 도달했을 때는 반드시 팔아야 한다.
-// O(N^2)으로 풀면 안되는데, 문제 조건을 안읽고 풀어서 WA
+typedef unsigned long long ull;
+int arr[1000002];
 
 int main(void) {
-    cin.tie(0)->sync_with_stdio(false);
+    cin.tie(0) -> sync_with_stdio(false);
 
     int t; cin >> t;
-    while (t--) {
+    while (t-- > 0) {
         int n; cin >> n;
-        vector<int> price(n);
-        long long earn = 0;
-        
-        for (int i = 0; i < n; i++) cin >> price[i];
-        // 앞에서부터 매번 맨 뒤까지 부분 최댓값을 찾으면서 순회하면 O(N^2) --> 비효율적
-        int j = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (price[i] < price[j]) earn += price[j] - price[i];
-            else j = i; // 최댓값 수정
+        for (int i = 0; i < n; i++) cin >> arr[i];
+
+        int max = 0;
+        ull earn = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] > max) max = arr[i];
+            else earn += (max - arr[i]);
         }
         cout << earn << '\n';
     }
