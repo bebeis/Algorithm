432e3467fa2d324dbffc0930a913dd83178f3090
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-04-12 11:44:16 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
@@ -1,55 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int result = 0;
-int tap[101];
-int arr[101];
+int n, k;
+int arr[102];
+int slot[102];
+int slotMap[102];
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int n, k;
-    queue<int> que[101];
+int findEmptySlot() {
+    for (int i = 1; i <= n; i++) {
+        if (slot[i] == 0) return i;
+    }
+    return -1;
+}
+
+int replace(int curTime) {
+    int targetSlot = -1;
+    int maxDistance = -1;
+
+    for (int i = 1; i <= n; i++) {
+        int item = slot[i];
+
+        int nextUse = 1e9;
+        for (int t = curTime + 1; t <= k; t++) {
+            if (arr[t] == item) {
+                nextUse = t;
+                break;
+            }
+        }
+
+        if (nextUse > maxDistance) {
+            maxDistance = nextUse;
+            targetSlot = i;
+        }
+    }
+
+    return targetSlot;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+
     cin >> n >> k;
-    for (int i = 0; i < k; i++) {
-        cin >> arr[i];
-        que[arr[i]].push(i);
-    }
-    for (int i = 0; i < k; i++) {
-        bool skip = false;
-        // 이미 멀티탭에 꽂혀있거나 비어있는 경우
-        for (int j = 0; j < n; j++) {
-            if (tap[j] == arr[i]) {
-                // cout << j << "번째 탭에 이미 " << arr[i] << "가 꽂혀있다.\n";
-                que[arr[i]].pop();
-                skip = true;
-                break;
-            } else if (tap[j] == 0) {
-                // cout << j << "번째 탭에 " << arr[i] << "를 꽂는다.\n";
-                tap[j] = arr[i];
-                que[arr[i]].pop();
-                skip = true;
-                break;
-            }
+    for (int i = 1; i <= k; i++) cin >> arr[i];
+
+    int cnt = 0;
+    for (int i = 1; i <= k; i++) {
+        int cur = arr[i];
+
+        if (slotMap[cur] != 0) continue;
+
+        int emptySlot = findEmptySlot();
+        if (emptySlot == -1) {
+            int slotId = replace(i);
+            slotMap[slot[slotId]] = 0;
+            slot[slotId] = cur;
+            slotMap[cur] = slotId;
+            cnt++;
+        } else {
+            slot[emptySlot] = cur;
+            slotMap[cur] = emptySlot;
         }
-        if (skip) continue;
-        // 가장 나중에 등장하는 콘센트를 빼야 함(아에 안나오면 그걸 빼면 됨)
-        int far_pos = -1;
-        int far_consent = -1;
-        for (int j = 0; j < n; j++) {
-            if (que[tap[j]].empty()) {
-                far_consent = j;
-                break;
-            }
-            else if (que[tap[j]].front() > far_pos) {
-                far_consent = j;
-                far_pos = que[tap[j]].front();
-            }
-        }
-        // cout << far_consent << "번째 탭에 " << tap[far_consent] << "를 빼고 " << arr[i] << "를 꽂는다.\n";
-        tap[far_consent] = arr[i];
-        que[arr[i]].pop();
-        result++;
     }
-    cout << result;
+
+    cout << cnt;
 }
