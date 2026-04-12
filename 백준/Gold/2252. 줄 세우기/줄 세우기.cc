df60e22cd7121e87e8e1dd5af55bb8ed7aee7a71
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-04-12 12:54:39 +0900

    [Gold III] Title: 줄 세우기, Time: 24 ms, Memory: 3956 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> adj[32001];
-int indeg[32001];
-int n, m;
+int indeg[32002];
+vector<int> adj[32002];
 
-int main(void) {
-    cin.tie(0)->sync_with_stdio(false);
-    cin >> n >> m;
-    while (m--) {
-        int v, e;
-        cin >> v >> e;
-        adj[v].push_back(e);
-        indeg[e]++;
+int main() {
+    cin.tie(0) -> sync_with_stdio(false);
+    int n, m; cin >> n >> m;
+    
+    while (m-- > 0) {
+        int a, b; cin >> a >> b;
+        indeg[b]++;
+
+        adj[a].push_back(b);
     }
+
     queue<int> q;
     for (int i = 1; i <= n; i++) {
         if (indeg[i] == 0) q.push(i);
     }
 
     while (!q.empty()) {
-        int cur = q.front();
+        auto cur = q.front();
         q.pop();
         cout << cur << ' ';
-        for (auto nxt : adj[cur]) {
+
+        for (int nxt : adj[cur]) {
             if (--indeg[nxt] == 0) q.push(nxt);
         }
     }
