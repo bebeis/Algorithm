271dec9e2423f80c2f008234770b2b3f0eec8ea9
Junseo Bae <69619752+bebeis@users.noreply.github.com>
2026-04-12 09:16:37 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,24 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int s[100001];
-int d[100001];
-
+int d[100002];
+int arr[100002];
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0) -> sync_with_stdio(false);
+    int n; cin >> n;
 
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++) cin >> s[i];
-    d[1] = s[1];
-    for (int i = 2; i <= n; i++) {
-        /* 코드 개선
-        if (d[i - 1] > 0) d[i] = d[i - 1] + s[i];
-        else d[i] = s[i];
-        위 코드를 아래로 줄일 수 있다.
-        */ 
-        d[i] = max(0, d[i - 1]) + s[i];
+    for (int i = 1; i <= n; i++) cin >> arr[i];
+
+    for (int i = 1; i <= n; i++) {
+        d[i] = max(d[i - 1] + arr[i], arr[i]);
     }
+
     cout << *max_element(d + 1, d + n + 1);
 }
